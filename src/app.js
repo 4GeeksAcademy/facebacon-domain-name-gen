ed2d9c0c9248bb58a4ca59ec252ca6383fa7317f
Sebastian Diaz <diazebas@gmail.com>
2024-07-26 19:13:48 +0000

    Add function to generate and log domain name combinations

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ["the", "our", "my", "your"];
+let adj = ["great", "big", "super", "fast"];
+let noun = ["jogger", "raccoon", "runner", "panda"];
+let extensions = [".com", ".net", ".us", ".io"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let generateDomains = (pronouns, adjectives, nouns, extensions) => {
+  let domains = [];
+  for (let p of pronouns) {
+    for (let a of adjectives) {
+      for (let n of nouns) {
+        for (let ext of extensions) {
+          domains.push(`${p}${a}${n}${ext}`);
+        }
+      }
+    }
+  }
+  return domains;
 };
+
+let domains = generateDomains(pronoun, adj, noun, extensions);
+domains.forEach(domain => console.log(domain));
